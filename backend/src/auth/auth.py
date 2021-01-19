e6013832c6966c53539f6497c34837dee63b7f40
fernando peguero <fernandoapeguero@gmail.com>
2021-01-19 15:38:34 -0500

    fix indentation remove comment and added reload to save coffe recipe

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -24,16 +24,6 @@ class AuthError(Exception):
 
 # Auth Header
 
-'''
-@TODO implement get_token_auth_header() method
-    it should attempt to get the header from the request
-        it should raise an AuthError if no header is present
-    it should attempt to split bearer and the token
-        it should raise an AuthError if the header is malformed
-    return the token part of the header
-'''
-
-
 def get_token_auth_header():
 
     auth = request.headers.get('Authorization', None)
@@ -68,19 +58,6 @@ def get_token_auth_header():
     return token
 
 
-'''
-@TODO implement check_permissions(permission, payload) method
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
-        payload: decoded jwt payload
-
-    it should raise an AuthError if permissions are not included in the payload
-        !!NOTE check your RBAC settings in Auth0
-    it should raise an AuthError if the requested permission string is not in the payload permissions array
-    return true otherwise
-'''
-
-
 def check_permissions(permission, payload):
 
     if 'permissions' not in payload:
@@ -96,21 +73,6 @@ def check_permissions(permission, payload):
         }, 401)
 
     return True
-
-
-'''
-@TODO implement verify_decode_jwt(token) method
-    @INPUTS
-        token: a json web token (string)
-
-    it should be an Auth0 token with key id (kid)
-    it should verify the token using Auth0 /.well-known/jwks.json
-    it should decode the payload from the token
-    it should validate the claims
-    return the decoded payload
-
-    !!NOTE urlopen has a common certificate error described here: https://stackoverflow.com/questions/50236117/scraping-ssl-certificate-verify-failed-error-for-http-en-wikipedia-org
-'''
 
 
 def verify_decode_jwt(token):
@@ -138,30 +100,27 @@ def verify_decode_jwt(token):
             )
             return payload
         except jwt.ExpiredSignatureError:
-            raise AuthError({"code": "token_expired",
-                            "description": "token is expired"}, 401)
-        except jwt.JWTClaimsError:
-            raise AuthError({"code": "invalid_claims",
-                            "description":
-                                "incorrect claims,"
-                                "please check the audience and issuer"}, 401)
-        except Exception:
-            raise AuthError({"code": "invalid_header",
-                            "description":
-                                "Unable to parse authentication"
-                                " token."}, 401)
-    raise AuthError({"code": "invalid_header",
-                    "description": "Unable to find appropriate key"}, 401)
-'''
-@TODO implement @requires_auth(permission) decorator method
-    @INPUTS
-        permission: string permission (i.e. 'post:drink')
+            raise AuthError({
+                "code": "token_expired",
+                "description": "token is expired"}, 401)
 
-    it should use the get_token_auth_header method to get the token
-    it should use the verify_decode_jwt method to decode the jwt
-    it should use the check_permissions method validate claims and check the requested permission
-    return the decorator which passes the decoded payload to the decorated method
-'''
+        except jwt.JWTClaimsError:
+            raise AuthError({
+                "code": "invalid_claims",
+                "description":
+                "incorrect claims,"
+                "please check the audience and issuer"}, 401)
+
+        except Exception:
+            raise AuthError({
+                "code": "invalid_header",
+                "description":
+                "Unable to parse authentication"
+                " token."}, 401)
+
+    raise AuthError({
+        "code": "invalid_header",
+        "description": "Unable to find appropriate key"}, 401)
 
 
 def requires_auth(permission=''):
