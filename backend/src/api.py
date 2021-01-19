fa26bd1e451dc448b9e013f68c06208f1e7737f4
fernando peguero <fernandoapeguero@gmail.com>
2021-01-18 19:45:38 -0500

    fix indentation issues and all is working as it should

--- backend/src/api.py
+++ backend/src/api.py
@@ -29,22 +29,13 @@ def get_drinks():
 
         for drink in drinks:
             results.append(drink.short())
-        
+
         return jsonify({
             'success': 200,
             'drinks': results
         }), 200
     except:
         abort(404)
-
-'''
-@TODO implement endpoint
-    GET /drinks-detail
-        it should require the 'get:drinks-detail' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drinks} where drinks is the list of drinks
-        or appropriate status code indicating reason for failure
-'''
 
 
 @app.route('/drinks-detail')
@@ -65,16 +56,6 @@ def get_drink_details(jwt):
     except:
         abort(404)
 
-'''
-@TODO implement endpoint
-    POST /drinks
-        it should create a new row in the drinks table
-        it should require the 'post:drinks' permission
-        it should contain the drink.long() data representation
-    returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the newly created drink
-        or appropriate status code indicating reason for failure
-'''
-
 
 @app.route('/drinks', methods=['POST'])
 @requires_auth('post:drinks')
@@ -85,15 +66,16 @@ def post_drinks(jwt):
 
         title = data['title']
         recipe = data['recipe']
-        drink = Drink(title=title , recipe=json.dumps(recipe))
+        drink = Drink(title=title, recipe=json.dumps(recipe))
         drink.insert()
-        
+
         return jsonify({
             'success': 200,
-            'drink': drink.long()
+            'drinks': drink.long()
         }), 200
     except:
         abort(400)
+
 
 '''
 @TODO implement endpoint
@@ -113,52 +95,48 @@ def post_drinks(jwt):
 def post_drink(jwt, drink_id):
 
     try:
+        data = request.get_json()
         print(drink_id)
-        # drink = Drink.query.filter(Drink.id == drink_id).all()
 
-        # if len(drink) == 0:
-        #     abort(404)
+        recipe = data['recipe']
+
+        drink = Drink.query.filter(Drink.id == drink_id).first()
+
+        if not drink:
+            abort(404)
+
+        drink.recipe = json.dumps(recipe)
+        drink.update()
 
         return jsonify({
             'success': True,
-            'drinks': []
+            'drinks': drink.long()
         }), 200
     except:
         abort(404)
 
 
-'''
-@TODO implement endpoint
-    DELETE /drinks/<id>
-        where <id> is the existing model id
-        it should respond with a 404 error if <id> is not found
-        it should delete the corresponding row for <id>
-        it should require the 'delete:drinks' permission
-    returns status code 200 and json {"success": True, "delete": id} where id is the id of the deleted record
-        or appropriate status code indicating reason for failure
-'''
-
-@app.route('/drinks/<int:drink_id>' , methods=['DELETE'])
+@app.route('/drinks/<int:drink_id>', methods=['DELETE'])
 @requires_auth('delete:drinks')
 def delete_drink(jwt, drink_id):
 
     try:
 
         drink = Drink.query.filter(Drink.id == drink_id).first()
-    
-        # if not drink:
-        #     abort(404)
-        
+
+        if not drink:
+            abort(404)
+
+        drink.delete()
+
         return jsonify({
             'success': True,
             'delete': drink_id
         }), 200
     except:
         abort(404)
+
 # Error Handling
-'''
-Example error handling for unprocessable entity
-'''
 
 
 @app.errorhandler(422)
@@ -170,17 +148,6 @@ def unprocessable(error):
     }), 422
 
 
-'''
-@TODO implement error handlers using the @app.errorhandler(error) decorator
-    each error handler should return (with approprate messages):
-             jsonify({
-                    "success": False, 
-                    "error": 404,
-                    "message": "resource not found"
-                    }), 404
-'''
-
-
 @app.errorhandler(404)
 def not_found(error):
 
@@ -189,18 +156,6 @@ def not_found(error):
         'error': 404,
         'message': 'resource not found'
     }), 404
-
-
-'''
-@TODO implement error handler for 404
-    error handler should conform to general task above 
-'''
-
-
-'''
-@TODO implement error handler for AuthError
-    error handler should conform to general task above 
-'''
 
 
 @app.errorhandler(AuthError)
